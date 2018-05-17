1730221bd27677691803a450b0b3efa50cf9ffe6
Gavin Price <gaprice@lbl.gov>
2018-05-17 16:54:40 -0700

    Add Minhash Impl name tests

--- src/us/kbase/assemblyhomology/minhash/MinHashImplementationName.java
+++ src/us/kbase/assemblyhomology/minhash/MinHashImplementationName.java
@@ -4,11 +4,17 @@ import us.kbase.assemblyhomology.core.Name;
 import us.kbase.assemblyhomology.core.exceptions.IllegalParameterException;
 import us.kbase.assemblyhomology.core.exceptions.MissingParameterException;
 
+/** The name of a MinHash implementation, like mash or sourmash.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class MinHashImplementationName extends Name {
 
-	//TODO JAVADOC
-	//TODO TEST
-	
+	/** Create the name.
+	 * @param id the name.
+	 * @throws MissingParameterException if the name is null or whitespace only.
+	 * @throws IllegalParameterException if the name is too long or contains control characters.
+	 */
 	public MinHashImplementationName(final String id)
 			throws MissingParameterException, IllegalParameterException {
 		super(id, "minhash implementation name", 256);
