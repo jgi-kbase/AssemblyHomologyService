59f5d946ca6f764f63cd5b2c4d6360f3eb262a98
Gavin Price <gaprice@lbl.gov>
2018-08-22 13:05:09 -0700

    Bump dev version

--- src/us/kbase/test/assemblyhomology/service/api/RootTest.java
+++ src/us/kbase/test/assemblyhomology/service/api/RootTest.java
@@ -17,7 +17,7 @@ import us.kbase.test.assemblyhomology.TestCommon;
 
 public class RootTest {
 	
-	public static final String SERVER_VER = "0.1.1-dev1";
+	public static final String SERVER_VER = "0.1.1-dev2";
 	private static final String GIT_ERR = 
 			"Missing git commit file gitcommit, should be in us.kbase.assemblyhomology";
 	
